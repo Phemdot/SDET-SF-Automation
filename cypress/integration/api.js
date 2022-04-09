4ddb56b36d1cde891fd023a56d8dbe2acbc07b69
FEMI OYEDOTUN <femi.oyedotun@gmail.com>
2022-04-09 15:03:50 +0100

    created data file for user

--- cypress/integration/api.js
+++ cypress/integration/api.js
@@ -1,19 +1,29 @@
 /// <reference types="cypress"/>
 
 it('Create new user', () => {
-    cy.request({
-        method: 'POST', 
-        url: 'https://reqres.in/api/users', 
+    
+    cy.fixture('user').then((userdata) =>{
 
-        body: {
-            'name' : 'Khalid',
-            'job' : 'Sec'
-            },
-        
-            headers: {
-                'content-type' : 'application/json'
-            }
-
-      })
+        cy.request({
+            method: 'POST', 
+            url: 'https://reqres.in/api/users', 
+    
+            body: {
+                "name" : userdata.name,
+                "job" :  userdata.job
+                },
+            
+                headers: {
+                    'content-type' : 'application/json'
+                }
+    
+          }).then((res) => {
+              expect(res.status).to.equal(201),
+              expect(res.duration).to.be.lessThan(2000),
+              expect(res.headers).to.have.property('content-type','application/json')
+          })
+    })
+    
+    
 
 })
